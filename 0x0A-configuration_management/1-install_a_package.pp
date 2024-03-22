5885611f8dbdf40eec9e3087ea19c05bed8b16f4
kponire <kponire@gmail.com>
2024-03-22 10:46:45 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install `flask` version `2.1.0` from `pip3`
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# installing puppet link
+package { 'puppet-lint':
+    ensure   => '2.1.1',
+    provider => 'gem',
 }
